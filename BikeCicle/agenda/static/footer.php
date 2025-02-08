01815bfecc47d398fc1d69c8138d1e6da589e955
guilhermegbs <djguilhermegb1983@gmail.com>
2025-02-07 21:34:55 -0300

    atualizacao 07/02/25

--- BikeCicle/agenda/static/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-<footer>
-    <br><br>
-    <p style="text-align: center;">&copy; <?php echo date('Y'); ?> AgendaAqui</p>
-</footer>
-</body>
-
-</html>
--- BikeCicle/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servidor = "localhost";
-    $dbususario = "root";
-    $dbsenha = "";
-    $dbname = "agenda";
-    $conn = mysqli_connect($servidor,$dbususario,$dbsenha,$dbname);
-?>
